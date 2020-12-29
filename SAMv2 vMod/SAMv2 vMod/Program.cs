053ceef9419fc49e9271c4da2428be068bf30713
Seth Chick <SCBionicle@gmail.com>
2020-12-28 18:50:02 -0600

    added inertial off on nonstatic grid dock

--- SAMv2 vMod/SAMv2 vMod/Program.cs
+++ SAMv2 vMod/SAMv2 vMod/Program.cs
@@ -3310,7 +3310,10 @@ namespace IngameScript
                     if (connector.Status == MyShipConnectorStatus.Connected)
                     {
                         connected = true;
-
+                        if (!connector.OtherConnector.CubeGrid.IsStatic)
+                        {
+                            RemoteControl.block.DampenersOverride = false;
+                        }
                     }
                 }
                 return connected;
